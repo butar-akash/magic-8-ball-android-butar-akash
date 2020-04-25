0197aacdd42def0229761da445b3bcd1ac47a85f
Akash <butar.akash@gmail.com>
2020-04-25 12:17:30 +0530

    Final Commit(The App is Completed)

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Declaring the variables
+        Button button_roll;
+        final ImageView image;
+        //Linking the variables to the objects
+        button_roll =(Button) findViewById(R.id.bu_roll);
+        image =(ImageView) findViewById(R.id.imageView_change);
+
+         final int [] Array = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+        //setting the on click listener
+        button_roll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(5);
+
+                image.setImageResource(Array[number]);
+            }
+        });
+
     }
 }
